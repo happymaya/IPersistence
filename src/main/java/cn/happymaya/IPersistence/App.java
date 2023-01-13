29f26ca44bace5e6a56f14b5dcedb15cd2861f7a
hsc <haoshichuan@foxmail.com>
2023-01-13 18:12:03 +0800

    first commit remark

---
+++ src/main/java/cn/happymaya/IPersistence/App.java
@@ -0,0 +1,13 @@
+package cn.happymaya.IPersistence;
+
+/**
+ * Hello world!
+ *
+ * @author super
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
